55d29e7b68b51bcbbc36d36bad687dbf5ced7c99
Louis Freier <Louisjamesfreier@gmail.com>
2026-01-20 22:00:05 +0000

    main.cpp: AST-Smoketest ohne Parser lauffähig gemacht

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
 
-#include "ast/program.hpp"
 #include "interp/env.hpp"
 #include "interp/exec.hpp"
 #include "interp/functions.hpp"
-#include "interp/methods.hpp"
-#include "sem/class_table.hpp"
+#include "ast/expr.hpp"
+
+static std::unique_ptr<ast::BinaryExpr> make_bin(
+    ast::BinaryExpr::Op op,
+    ast::ExprPtr lhs,
+    ast::ExprPtr rhs
+) {
+    auto b = std::make_unique<ast::BinaryExpr>();
+    b->op = op;
+    b->left = std::move(lhs);
+    b->right = std::move(rhs);
+    return b;
+}
 
 int main() {
-    ast::Program p;
+    using namespace ast;
+    using namespace interp;
 
-    interp::Env global;
-    interp::FunctionTable ft;
-    interp::MethodTable methods;
-    sem::ClassTable classes;
+    Env env(nullptr);
+    FunctionTable functions;
 
-    (void)p;
-    (void)global;
-    (void)ft;
-    (void)methods;
-    (void)classes;
+    // AST: 1 + (2 * 3)
+    ExprPtr expr = make_bin(
+        BinaryExpr::Op::Add,
+        std::make_unique<IntLiteral>(1),
+        make_bin(
+            BinaryExpr::Op::Mul,
+            std::make_unique<IntLiteral>(2),
+            std::make_unique<IntLiteral>(3)
+        )
+    );
+
+    try {
+        Value v = eval_expr(env, *expr, functions);
+        std::cout << "Interpreter-Test OK, Ergebnis = " << std::get<int>(v) << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << "Interpreter-Test FEHLER: " << e.what() << std::endl;
+        return 1;
+    }
 
     return 0;
 }
