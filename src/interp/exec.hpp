bb1102255a74611e03045d5c5743d5963ea319f7
Louis Freier <Louisjamesfreier@gmail.com>
2026-01-20 21:54:56 +0000

    Interpreter: Operatoren vervollständigt und LValue-Zugriff repariert

--- src/interp/exec.hpp
+++ src/interp/exec.hpp
@@ -2,6 +2,7 @@
 #include <stdexcept>
 #include <vector>
 #include <iostream>
+#include <string>
 
 #include "env.hpp"
 #include "functions.hpp"
@@ -33,6 +34,16 @@ inline int expect_int(const Value& v, const char* ctx) {
 inline bool expect_bool(const Value& v, const char* ctx) {
     if (auto* pb = std::get_if<bool>(&v)) return *pb;
     throw std::runtime_error(std::string("type error: expected bool in ") + ctx);
+}
+
+inline char expect_char(const Value& v, const char* ctx) {
+    if (auto* pc = std::get_if<char>(&v)) return *pc;
+    throw std::runtime_error(std::string("type error: expected char in ") + ctx);
+}
+
+inline const std::string& expect_string(const Value& v, const char* ctx) {
+    if (auto* ps = std::get_if<std::string>(&v)) return *ps;
+    throw std::runtime_error(std::string("type error: expected string in ") + ctx);
 }
 
 inline Value eval_expr(Env& env, const ast::Expr& e, FunctionTable& functions);
@@ -125,7 +136,11 @@ inline Value eval_expr(Env& env, const ast::Expr& e, FunctionTable& functions) {
     }
 
     if (auto* fa = dynamic_cast<const FieldAssignExpr*>(&e)) {
-        LValue lv = eval_lvalue(env, e, functions);
+        Value objv = eval_expr(env, *fa->object, functions);
+        auto* pobj = std::get_if<ObjectPtr>(&objv);
+        if (!pobj || !*pobj)
+            throw std::runtime_error("field assignment on non-object");
+        LValue lv = LValue::field_of(*pobj, fa->field);
         Value rhs = eval_expr(env, *fa->value, functions);
         env.write_lvalue(lv, rhs);
         return rhs;
@@ -157,18 +172,64 @@ inline Value eval_expr(Env& env, const ast::Expr& e, FunctionTable& functions) {
         Value lv = eval_expr(env, *bin->left, functions);
         Value rv = eval_expr(env, *bin->right, functions);
 
-        if (bin->op == BinaryExpr::Op::Add) return expect_int(lv, "+") + expect_int(rv, "+");
-        if (bin->op == BinaryExpr::Op::Sub) return expect_int(lv, "-") - expect_int(rv, "-");
-        if (bin->op == BinaryExpr::Op::Mul) return expect_int(lv, "*") * expect_int(rv, "*");
-        if (bin->op == BinaryExpr::Op::Div) {
-            int r = expect_int(rv, "/");
-            if (r == 0) throw std::runtime_error("Division durch 0");
-            return expect_int(lv, "/") / r;
-        }
-        if (bin->op == BinaryExpr::Op::Mod) {
-            int r = expect_int(rv, "%");
-            if (r == 0) throw std::runtime_error("Modulo durch 0");
-            return expect_int(lv, "%") % r;
+        switch (bin->op) {
+            case BinaryExpr::Op::Add: return expect_int(lv, "+") + expect_int(rv, "+");
+            case BinaryExpr::Op::Sub: return expect_int(lv, "-") - expect_int(rv, "-");
+            case BinaryExpr::Op::Mul: return expect_int(lv, "*") * expect_int(rv, "*");
+            case BinaryExpr::Op::Div: {
+                int r = expect_int(rv, "/");
+                if (r == 0) throw std::runtime_error("Division durch 0");
+                return expect_int(lv, "/") / r;
+            }
+            case BinaryExpr::Op::Mod: {
+                int r = expect_int(rv, "%");
+                if (r == 0) throw std::runtime_error("Modulo durch 0");
+                return expect_int(lv, "%") % r;
+            }
+
+            case BinaryExpr::Op::Lt:
+            case BinaryExpr::Op::Le:
+            case BinaryExpr::Op::Gt:
+            case BinaryExpr::Op::Ge: {
+                if (std::holds_alternative<int>(lv)) {
+                    int a = expect_int(lv, "relop");
+                    int b = expect_int(rv, "relop");
+                    if (bin->op == BinaryExpr::Op::Lt) return a < b;
+                    if (bin->op == BinaryExpr::Op::Le) return a <= b;
+                    if (bin->op == BinaryExpr::Op::Gt) return a > b;
+                    return a >= b;
+                }
+                if (std::holds_alternative<char>(lv)) {
+                    char a = expect_char(lv, "relop");
+                    char b = expect_char(rv, "relop");
+                    if (bin->op == BinaryExpr::Op::Lt) return a < b;
+                    if (bin->op == BinaryExpr::Op::Le) return a <= b;
+                    if (bin->op == BinaryExpr::Op::Gt) return a > b;
+                    return a >= b;
+                }
+                throw std::runtime_error("type error: relational operators only for int/char");
+            }
+
+            case BinaryExpr::Op::Eq:
+            case BinaryExpr::Op::Ne: {
+                bool eq = false;
+                if (std::holds_alternative<int>(lv)) {
+                    eq = expect_int(lv, "==") == expect_int(rv, "==");
+                } else if (std::holds_alternative<char>(lv)) {
+                    eq = expect_char(lv, "==") == expect_char(rv, "==");
+                } else if (std::holds_alternative<bool>(lv)) {
+                    eq = expect_bool(lv, "==") == expect_bool(rv, "==");
+                } else if (std::holds_alternative<std::string>(lv)) {
+                    eq = expect_string(lv, "==") == expect_string(rv, "==");
+                } else {
+                    throw std::runtime_error("type error: ==/!= not supported for this type");
+                }
+                return (bin->op == BinaryExpr::Op::Eq) ? eq : !eq;
+            }
+
+            case BinaryExpr::Op::AndAnd:
+            case BinaryExpr::Op::OrOr:
+                break;
         }
     }
 
